f89ccdc77c36c64eb35f25a25395a2f5fae485af
Farischa M <farischamakay09@gmail.com>
2025-07-08 10:14:29 +0700

    add export sdk for card

--- lib/widgets/card/balance_tl_type/tpe_balance_card_tl_type.dart
+++ lib/widgets/card/balance_tl_type/tpe_balance_card_tl_type.dart
@@ -5,7 +5,6 @@ import 'package:tpe_component_sdk/widgets/card/balance_tl_type/tpe_account_secti
 import 'package:tpe_component_sdk/widgets/card/balance_tl_type/tpe_balance_card_section.dart';
 import 'package:tpe_component_sdk/widgets/card/model/tpe_balance_card_data.dart';
 
-
 class TPEBalanceCardTLType extends StatefulWidget {
   final TpeBalanceCardData data;
   final VoidCallback? onCopyAccountNumber;
@@ -16,6 +15,7 @@ class TPEBalanceCardTLType extends StatefulWidget {
   final Duration animationDuration;
   final BorderRadius? borderRadius;
   final EdgeInsets? padding;
+  final EdgeInsets? marginContainer;
   final Color? backgroundColor;
   final Color? secondaryBackgroundColor;
   final Color? textColor;
@@ -31,6 +31,7 @@ class TPEBalanceCardTLType extends StatefulWidget {
     this.showBalanceToggle = true,
     this.animationDuration = const Duration(milliseconds: 300),
     this.borderRadius,
+    this.marginContainer,
     this.padding,
     this.backgroundColor,
     this.secondaryBackgroundColor,
@@ -89,39 +90,43 @@ class _BalanceCardState extends State<TPEBalanceCardTLType>
   @override
   Widget build(BuildContext context) {
     final borderRadius = widget.borderRadius ?? BorderRadius.circular(16);
+    final marginContainer = widget.marginContainer ?? const EdgeInsets.all(16);
     final padding = widget.padding ?? const EdgeInsets.all(16);
     final bgColor = widget.backgroundColor ?? TPEColors.blue100;
     final secondaryBg = widget.secondaryBackgroundColor ?? TPEColors.blue80;
     final textColor = widget.textColor ?? TPEColors.white;
     final secondaryText = widget.secondaryTextColor ?? TPEColors.ligth30;
 
-    return Column(
-      children: [
-        AccountSectionTLTypeCard(
-          data: widget.data,
-          padding: padding,
-          borderRadius: borderRadius,
-          backgroundColor: bgColor,
-          textColor: textColor,
-          secondaryTextColor: secondaryText,
-          onCopy: _handleCopy,
-          copyIconPath: widget.copyIconPath,
-        ),
-        BalanceSectionTLTypeCard(
-          data: widget.data,
-          balanceVisible: _balanceVisible,
-          onToggleVisibility: widget.showBalanceToggle
-              ? _toggleBalanceVisibility
-              : null,
-          padding: padding,
-          borderRadius: borderRadius,
-          backgroundColor: secondaryBg,
-          textColor: textColor,
-          secondaryTextColor: secondaryText,
-          fadeAnimation: _fadeAnimation,
-          balanceFormatter: widget.balanceFormatter,
-        ),
-      ],
+    return Container(
+      margin: marginContainer,
+      child: Column(
+        children: [
+          AccountSectionTLTypeCard(
+            data: widget.data,
+            padding: padding,
+            borderRadius: borderRadius,
+            backgroundColor: bgColor,
+            textColor: textColor,
+            secondaryTextColor: secondaryText,
+            onCopy: _handleCopy,
+            copyIconPath: widget.copyIconPath,
+          ),
+          BalanceSectionTLTypeCard(
+            data: widget.data,
+            balanceVisible: _balanceVisible,
+            onToggleVisibility: widget.showBalanceToggle
+                ? _toggleBalanceVisibility
+                : null,
+            padding: padding,
+            borderRadius: borderRadius,
+            backgroundColor: secondaryBg,
+            textColor: textColor,
+            secondaryTextColor: secondaryText,
+            fadeAnimation: _fadeAnimation,
+            balanceFormatter: widget.balanceFormatter,
+          ),
+        ],
+      ),
     );
   }
 }
